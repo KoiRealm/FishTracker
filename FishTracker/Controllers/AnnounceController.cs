c5a138d000adaf1aab9a526f080bef52aa646a8c
Cyrus Vance <493093054@qq.com>
2022-05-25 22:40:57 +0800

    Add some test method

---
+++ FishTracker/Controllers/AnnounceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FishTracker.Controllers
+{
+    [Route("api/announce")]
+    [ApiController]
+    public class AnnounceController : ControllerBase
+    {
+        [HttpGet("test")]
+        public ActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
