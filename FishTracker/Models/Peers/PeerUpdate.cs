7aea8bdc3041725b62b6ce0734ff56d982140b95
RTknight <863068893@qq.com>
2022-06-18 23:31:06 +0800

    create database, manage to update peer.

---
+++ FishTracker/Models/Peers/PeerUpdate.cs
@@ -0,0 +1,16 @@
+﻿using FishTracker.Helpers;
+using FishTracker.Models.API;
+
+namespace FishTracker.Models.Peers
+{
+    public class PeerUpdate
+    {
+        public static void PeerUpdateToDB(GetPeersObject apiInput, string connStr, int dbId)
+        {
+            AnnounceInputParameters announceInputParameters = new AnnounceInputParameters(apiInput);
+            Peer peer = new Peer(announceInputParameters);
+            
+        }
+
+    }
+}
